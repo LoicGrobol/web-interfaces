c76a7901a475b9677fee5c5d38c7a4a33ecad9cb
LoicGrobol <LoicGrobol@users.noreply.github.com>
2021-09-28 17:53:30 +0000

    Deploying to gh-pages from @ LoicGrobol/web-interfaces@3e9de68e0bd5d4e773013704c2a20972babaf134 🚀

---
+++ corrections/codingame_shadows-of-the-knight-episode-1.py
@@ -0,0 +1,35 @@
+import sys
+import math
+
+# Auto-generated code below aims at helping you parse
+# the standard input according to the problem statement.
+
+# w: width of the building.
+# h: height of the building.
+w, h = [int(i) for i in input().split()]
+n = int(input())  # maximum number of turns before game over.
+x0, y0 = [int(i) for i in input().split()]
+
+xmin = 0
+ymin = 0
+xmax = w
+ymax = h
+
+# game loop
+while True:
+    bomb_dir = input()  # the direction of the bombs from batman's current location (U, UR, R, DR, D, DL, L or UL)
+
+    # Write an action using print
+    # To debug: print("Debug messages...", file=sys.stderr)
+    if 'U' in bomb_dir:
+        ymax = y0
+    if 'D' in bomb_dir:
+        ymin = y0
+    if 'L' in bomb_dir:
+        xmax = x0
+    if 'R'in bomb_dir:
+        xmin = x0
+    
+    x0 = (xmin + xmax) // 2
+    y0 = (ymin + ymax) // 2
+    print(x0, y0)
