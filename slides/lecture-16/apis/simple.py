8ebe16d1ae36a91f75490ec7424e6bc2218b2a61
LoicGrobol <LoicGrobol@users.noreply.github.com>
2021-11-16 14:56:16 +0000

    Deploying to gh-pages from @ LoicGrobol/web-interfaces@082017eb22d0b97006c3639bab3c6bc8b42249e3 🚀

---
+++ slides/lecture-16/apis/simple.py
@@ -0,0 +1,80 @@
+from typing import List
+import sqlite3
+
+from fastapi import FastAPI, Depends
+from pydantic import BaseModel
+
+app = FastAPI()
+
+
+def get_db():
+    con = sqlite3.connect("db.sqlite3")
+    cur = con.cursor()
+    try:
+        yield cur
+        con.commit()
+    finally:
+        con.close()
+
+
+# Database setup
+con = sqlite3.connect("db.sqlite3")
+cur = con.cursor()
+cur.execute(
+    "create table if not exists trees (tree_id VARCHAR NOT NULL PRIMARY KEY, text TEXT)"
+)
+con.close()
+
+
+class Tree(BaseModel):
+    tree_id: str
+    text: str
+
+
+# Methods for interacting with the database
+def get_tree(cur: sqlite3.Cursor, tree_id: str):
+    cur.execute("select * from trees where tree_id=:tree_id", {"tree_id": tree_id})
+    db_tree_id, db_text = cur.fetchone()
+    return {"tree_id": db_tree_id, "text": db_text}
+
+
+def get_trees(cur: sqlite3.Cursor):
+    cur.execute("select * from trees")
+    return [
+        {"tree_id": db_tree_id, "text": db_text}
+        for db_tree_id, db_text in cur.fetchall()
+    ]
+
+
+def create_tree(cur: sqlite3.Cursor, tree: Tree):
+    cur.execute(
+        "insert into trees values (:tree_id, :text)",
+        {"tree_id": tree.tree_id, "text": tree.text},
+    )
+    return tree
+
+
+@app.post("/trees/", response_model=Tree)
+def create_trees_view(tree: Tree, db: sqlite3.Cursor = Depends(get_db)):
+    db_tree = create_tree(db, tree)
+    return db_tree
+
+
+@app.get("/trees/", response_model=List[Tree])
+def get_trees_view(db: sqlite3.Cursor = Depends(get_db)):
+    return get_trees(db)
+
+
+@app.get("/tree/{tree_id}")
+def get_tree_view(tree_id: str, db: sqlite3.Cursor = Depends(get_db)):
+    return get_tree(db, tree_id)
+
+
+@app.post("/trees/")
+async def create_tree_view(tree: Tree):
+    return tree
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World!"}
---
+++ slides/lecture-16/lecture-16.md
@@ -0,0 +1,31 @@
+---
+jupyter:
+  jupytext:
+    formats: ipynb,md
+    split_at_heading: true
+    text_representation:
+      extension: .md
+      format_name: markdown
+      format_version: '1.3'
+      jupytext_version: 1.13.1
+  kernelspec:
+    display_name: Python 3 (ipykernel)
+    language: python
+    name: python3
+---
+
+<!-- LTeX: language=fr -->
+
+<!-- #region slideshow={"slide_type": "slide"} -->
+Cours 16 : Bases de données relationnelles avec FastAPI
+=======================================================
+
+**Loïc Grobol** [<lgrobol@parisnanterre.fr>](mailto:lgrobol@parisnanterre.fr)
+
+2021-11-17
+<!-- #endregion -->
+
+
+```python
+from IPython.display import display
+```
