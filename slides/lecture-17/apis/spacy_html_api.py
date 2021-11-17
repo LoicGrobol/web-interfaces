609ce4349febd95d23d8c2c2aac47bd463a92f3f
LoicGrobol <LoicGrobol@users.noreply.github.com>
2021-11-17 11:57:20 +0000

    Deploying to gh-pages from @ LoicGrobol/web-interfaces@7fecca24b35138478e4d9d66c8d20570ede5b0e5 🚀

---
+++ slides/lecture-17/apis/spacy_html_api.py
@@ -0,0 +1,46 @@
+from fastapi import FastAPI, HTTPException
+from fastapi.staticfiles import StaticFiles
+from pydantic import BaseModel
+from fastapi.responses import HTMLResponse
+import spacy
+
+app = FastAPI()
+
+
+app.mount("/front", StaticFiles(directory="static"), name="front")
+
+
+class InputData(BaseModel):
+    sentence: str
+
+
+@app.post("/postag")
+async def postag(inpt: InputData, model="fr_core_news_sm"):
+    if model not in spacy.util.get_installed_models():
+        raise HTTPException(status_code=422, detail=f"Model {model!r} unavailable")
+    nlp = spacy.load(model)
+    doc = nlp(inpt.sentence)
+    above = """<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <title>This is what you asked me to display</title>
+  </head>
+  <body>
+  <ol>
+"""
+    below = """
+  </ol>
+  </body>
+</html>
+"""
+    lst = "\n".join([f"<li>{w.text}: {w.pos_}</li>" for w in doc])
+    html_content = "\n".join([above, lst, below])
+    return HTMLResponse(content=html_content, status_code=200)
+
+
+@app.get("/list")
+async def list_models():
+    return {"models": spacy.util.get_installed_models()}
+
+
